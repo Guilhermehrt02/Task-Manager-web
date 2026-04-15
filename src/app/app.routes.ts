64835a7adb37e1f5778fbb0c9975d80fb3d59a74
Guilherme <ghorita.gh@gmail.com>
2026-04-14 23:50:26 -0300

    feat(auth): add login component and routing
    - Implemented routing for login with a redirect from the root path.
    - Created AuthModule to encapsulate authentication-related components.
    - Developed LoginComponent with a user interface for login.
    - Added basic Auth service for future authentication logic.
    - Included tests for Auth service and LoginComponent.
    - Updated global styles to include Tailwind CSS.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
